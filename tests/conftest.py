5e970fa9a7eab4f44b84a48ddae20408adb61d4a
Florian Posdziech <hallo@flowfx.de>
2019-03-26 10:06:51 +0100

    Use flake8 to improve code quality

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
+"""Pytest fixtures."""
 import pytest
 from click.testing import CliRunner
 
 
 @pytest.fixture(scope='module')
 def isolated_filesystem_runner():
+    """Fixture to run a test in a temporary and isolated filesystem."""
     runner = CliRunner()
     with runner.isolated_filesystem():
         yield runner
--- tests/func/test_cli.py
+++ tests/func/test_cli.py
@@ -1,3 +1,4 @@
+"""System level tests of the fileget module."""
 import os
 
 from fileget import fileget
@@ -7,12 +8,12 @@ def test_fileget(isolated_filesystem_runner):
     images = ("https://flowfx.de/images/flowfx.jpg\n"
               "https://flowfx.de/images/flowfx.thumbnail.jpg")
 
-    with open('images.txt', 'w') as f:
-        f.write(images)
+    with open('images.txt', 'w') as file:
+        file.write(images)
 
     result = isolated_filesystem_runner.invoke(fileget, ['images.txt'])
     assert result.exit_code == 0
     assert result.output == f'{images}\n'
 
-    assert os.path.isfile('flowfx.jpg') == True
-    assert os.path.isfile('flowfx.thumbnail.jpg') == True
+    assert os.path.isfile('flowfx.jpg')
+    assert os.path.isfile('flowfx.thumbnail.jpg')
