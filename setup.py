9bd09fc18352b21fc9f9ca4439542a55369679df
Florian Posdziech <hallo@flowfx.de>
2019-03-26 09:28:49 +0100

    Create project structure suitable for a distributable package
    This follows the project structure outlined in
    https://pragprog.com/book/bopytest/python-testing-with-pytest

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fileget',
+    version='0.1.0',
+    license='GPL3',
+    description='Download images from the internets',
+
+    author='Florian Posdziech',
+    author_email='fileget-718498@flowfx.de',
+    url='https://flowfx.de',
+
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+
+    install_requires=['click', 'requests'],
+
+    # entry_points={
+        # 'console_scripts': [
+            # 'tasks = tasks.cli:tasks_cli',
+        # ]
+    # },
+)
